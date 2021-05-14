2a56ebfcbd23d348247090702534595e9da7b21d
Taegan Loyzelle <tloyzelle@gmail.com>
2021-05-14 14:35:36 -0400

    creating settings for password generator

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-// Assignment Code
+//variables for password
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var symbols = "!@#$%^&*()_+";
+
+var passwordTxt = document.getElementById("password");
+var length = document.getElementById("length");
+var incNumbers = document.getElementById("numbers");
+var incSymbols = document.getElementById("symbols");
+//assignment code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//event listener for generate button
+generateBtn.addEventListener("click", () => {
+  let characters = alpha;
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordTxt.value = generatePassword(length.value, characters);
+});
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//checking my git push
+//how it genreates the password
+var generatePassword = (length, characters) => {
+  let password = "";
+  for (let i=0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+};
