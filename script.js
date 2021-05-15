ee7f8638af0d9fbfe1a5f77b77f944c7616b112a
Taegan Loyzelle <tloyzelle@gmail.com>
2021-05-14 20:52:47 -0400

    updating character type options

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 //variables for password
-var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alpha = "";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
 var symbols = "!@#$%^&*()_+";
 
 var passwordTxt = document.getElementById("password");
 var length = document.getElementById("length");
+var incUpperCase = document.getElementById("upperCase");
+var incLowerCase = document.getElementById("lowerCase");
 var incNumbers = document.getElementById("numbers");
 var incSymbols = document.getElementById("symbols");
 //assignment code
 var generateBtn = document.querySelector("#generate");
-
 //event listener for generate button
 generateBtn.addEventListener("click", () => {
   let characters = alpha;
+  incUpperCase.checked ? (characters += upperCase) : "";
+  incLowerCase.checked ? (characters += lowerCase) : "";
   incNumbers.checked ? (characters += numbers) : "";
   incSymbols.checked ? (characters += symbols) : "";
   passwordTxt.value = generatePassword(length.value, characters);
